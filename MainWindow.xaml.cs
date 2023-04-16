b19fba454058e788717c8050bb363eb351ce7bbf
Aniruddha Mukherjee <aniruddha.mukherji@outlook.com>
2023-04-17 03:56:59 +0530

    game worlks!

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,38 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace AnimalGame
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    using System.Windows.Threading;
+
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
         public MainWindow()
         {
             InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text += " - Play Again?";
+            }
         }
 
         private void SetUpGame()
         {
-            Random random = new Random();
-            List<string> animals = new List<string>()
+            matchesFound = 0;
+            Random random = new();
+            List<string> animals = new()
             {
-                "🐕‍", "🐕",
-                "‍🐩", "🐩",
+                "🐕", "🐕",
+                "🐩", "🐩",
                 "🐎", "🐎",
                 "🐂", "🐂",
                 "🦛", "🦛",
@@ -40,12 +49,53 @@ namespace AnimalGame
                 "🦙", "🦙",
                 "🦣", "🦣"
             };
-            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                int index = random.Next(animals.Count);
-                string nextEmoji = animals[index];
-                textBlock.Text = nextEmoji;
-                animals.RemoveAt(index);
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    int index = random.Next(animals.Count);
+                    string nextEmoji = animals[index];
+                    textBlock.Text = nextEmoji;
+                    animals.RemoveAt(index);
+                }
+            }
+
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = (TextBlock)sender;
+            if (!findingMatch)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (lastTextBlockClicked.Text == textBlock.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+                matchesFound++;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame();
             }
         }
     }
